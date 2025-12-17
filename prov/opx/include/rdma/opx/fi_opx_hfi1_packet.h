f9a5d589b7123d4a0d22809bd3399aac93826640
Lindsay Reiser <lindsay.reiser@cornelisnetworks.com>
2025-12-17 14:13:08 -0500

    prov/opx: DMABUF support for HFI service
    * prov/opx: Internal DMABUF support for HFI service
    This enables DMABUF support in OPX for FI_OPX_HFISVC=1 and FI_HMEM_ROCR/CUDA_USE_DMABUF=1
    
    Signed-off-by: Lindsay Reiser <lindsay.reiser@cornelisnetworks.com>
    Co-authored-by: Mike Wilkins <mwilkins@cornelisnetworks.com>
    (cherry picked from commit a548352be3c988898bdbf895a10415b0c6ba51c1)

--- prov/opx/include/rdma/opx/fi_opx_hfi1_packet.h
+++ prov/opx/include/rdma/opx/fi_opx_hfi1_packet.h
@@ -1525,7 +1525,7 @@ union opx_hfisvc_iov {
 	};
 } __attribute__((__packed__));
 
-#define OPX_MAX_HFISVC_IOVS (OPX_HFI1_MAX_PKT_SIZE / sizeof(union opx_hfisvc_iov))
+#define OPX_MAX_HFISVC_IOVS ((OPX_HFI1_MAX_PKT_SIZE - sizeof(uintptr_t)) / sizeof(union opx_hfisvc_iov))
 
 struct fi_opx_hmem_iov {
 	uintptr_t	   buf;
@@ -1637,6 +1637,7 @@ union fi_opx_hfi1_packet_payload {
 		} ipc;
 
 		struct {
+			uintptr_t	     rzv_comp_vaddr; // struct fi_opx_rzv_completion * - imm_data for notify
 			union opx_hfisvc_iov iovs[OPX_MAX_HFISVC_IOVS];
 		} hfisvc;
 	} rendezvous;
