cornelisnetworks/libfabric

commit 845e42673dfa356c858713e2f69d26e91278880b

prov/opx: Dual plane: Multiple tx and ibv context
This is the first set of changes for dual plane. Changes include plane discovery and creating tx context and ibv_contexts per plane.

Signed-off-by: Archana Venkatesha <archana.venkatesha@cornelisnetworks.com>

prov/opx/include/rdma/opx/fi_opx_domain.h
@@ -64,6 +64,15 @@ struct fi_opx_ep; /* forward declaration */
 struct opx_tid_fabric;
 struct opx_hmem_fabric;
 
+#if HAVE_HFISVC
+struct fi_opx_hfisvc_ctx {
+	struct ibv_context *ctx;
+	opx_hfisvc_keyset_t access_key_set;
+	hfisvc_client_key_t client_key;
+	uint32_t	    padding;
+};
+#endif
+
 struct fi_opx_fabric {
 	struct fid_fabric fabric_fid;
 
@@ -113,7 +122,8 @@ struct fi_opx_domain {
 
 #if HAVE_HFISVC
 	struct {
-		struct ibv_context *ctx;
+		struct fi_opx_hfisvc_ctx ctxs[OPX_MAX_TX_CONTEXTS];
+		int			 num_ctxs;
 		/**
 		 * @brief Command queue used by the domain for issuing commands to the
 		 * hfisvc where we are opening/closing hfisvc memory regions.
@@ -125,10 +135,7 @@ struct fi_opx_domain {
 		 * hfisvc where we are opening/closing hfisvc memory regions.
 		 */
 		hfisvc_client_completion_queue_t mr_completion_queue;
-		opx_hfisvc_keyset_t		 access_key_set;
 		ofi_atomic64_t			 ref_cnt;
-		hfisvc_client_key_t		 client_key;
-		uint32_t			 padding;
 		void				*libhfi1verbs;
 		int (*initialize)(struct ibv_context *ctx);
 		int (*get_client_key)(struct ibv_context *ctx, hfisvc_client_key_t *key);
@@ -383,6 +390,7 @@ __OPX_FORCE_INLINE__
 void opx_domain_hfisvc_poll(struct fi_opx_domain *opx_domain)
 {
 	struct hfisvc_client_cq_entry hfisvc_out[64];
+
 	size_t n = (*opx_domain->hfisvc.cq_read)(opx_domain->hfisvc.mr_completion_queue, 0ul /* flags */, hfisvc_out,
 						 sizeof(struct hfisvc_client_cq_entry) * 64, 64);
 	while (n > 0) {
@@ -453,8 +461,8 @@ void opx_domain_hfisvc_poll(struct fi_opx_domain *opx_domain)
 				/* free the rendezvous completion structure */
 				OPX_BUF_FREE(rzv_comp);
 			} else if (opx_mr->hfisvc.state == OPX_MR_HFISVC_STATE_PENDING_KEY_DISABLE) {
-				opx_hfisvc_keyset_free_key(opx_domain->hfisvc.access_key_set, opx_mr->hfisvc.access_key,
-							   NULL);
+				opx_hfisvc_keyset_free_key(opx_domain->hfisvc.ctxs[0].access_key_set,
+							   opx_mr->hfisvc.access_key, NULL);
 				opx_mr->hfisvc.access_key = (uint32_t) -1;
 				OPX_HFISVC_DEBUG_LOG(
 					"MR State transition opx_mr=%p state=PENDING_KEY_DISABLE -> PENDING_DEREGISTER\n",
@@ -502,6 +510,7 @@ void opx_domain_hfisvc_poll(struct fi_opx_domain *opx_domain)
 }
 
 int opx_domain_hfisvc_init(struct fi_opx_domain *domain);
+int opx_domain_hfisvc_init_ctx(struct fi_opx_domain *domain, int ctx_index);
 #endif
 
 #ifdef __cplusplus

prov/opx/src/fi_opx_domain.c
@@ -86,8 +86,11 @@ static int fi_opx_close_domain(fid_t fid)
 
 #if HAVE_HFISVC
 	if (opx_domain->use_hfisvc) {
-		while (opx_hfisvc_keyset_outstanding(opx_domain->hfisvc.access_key_set)) {
-			opx_domain_hfisvc_poll(opx_domain);
+		/* Drain outstanding access keys for all contexts */
+		for (int i = 0; i < opx_domain->hfisvc.num_ctxs; i++) {
+			while (opx_hfisvc_keyset_outstanding(opx_domain->hfisvc.ctxs[i].access_key_set)) {
+				opx_domain_hfisvc_poll(opx_domain);
+			}
 		}
 	}
 #endif
@@ -114,11 +117,31 @@ static int fi_opx_close_domain(fid_t fid)
 			return ret;
 		}
 
-		opx_hfisvc_keyset_free(opx_domain->hfisvc.access_key_set);
+		/* Close domain-level MR queues */
+		ret = (*opx_domain->hfisvc.completion_queue_close)(&opx_domain->hfisvc.mr_completion_queue);
+		if (ret) {
+			FI_WARN(fi_opx_global.prov, FI_LOG_DOMAIN,
+				"[HFISVC] Failed closing domain MR completion queue, ret=%d\n", ret);
+		}
 
-		/* Close HFISVC context, the endpoint should already be closed */
-		assert(opx_domain->hfisvc.ctx != NULL);
-		opx_hfi1_rdma_context_close(opx_domain->hfisvc.ctx);
+		ret = (*opx_domain->hfisvc.command_queue_close)(&opx_domain->hfisvc.mr_command_queue);
+		if (ret) {
+			FI_WARN(fi_opx_global.prov, FI_LOG_DOMAIN,
+				"[HFISVC] Failed closing domain MR command queue, ret=%d\n", ret);
+		}
+
+		/* Close all hfisvc contexts */
+		for (int i = 0; i < opx_domain->hfisvc.num_ctxs; i++) {
+			if (opx_domain->hfisvc.ctxs[i].ctx == NULL) {
+				continue;
+			}
+
+			FI_INFO(fi_opx_global.prov, FI_LOG_DOMAIN, "[HFISVC] Closing context %d\n", i);
+
+			opx_hfisvc_keyset_free(opx_domain->hfisvc.ctxs[i].access_key_set);
+			opx_hfi1_rdma_context_close(opx_domain->hfisvc.ctxs[i].ctx);
+			opx_domain->hfisvc.ctxs[i].ctx = NULL;
+		}
 	}
 #endif
 
@@ -630,6 +653,44 @@ err:
 
 extern struct opx_rdma_ops_struct opx_rdma_ops;
 
+int opx_domain_hfisvc_init_ctx(struct fi_opx_domain *domain, int ctx_index)
+{
+	assert(ctx_index >= 0 && ctx_index < OPX_MAX_TX_CONTEXTS);
+	assert(domain->hfisvc.ctxs[ctx_index].ctx != NULL);
+
+	int ret;
+
+	FI_INFO(fi_opx_global.prov, FI_LOG_DOMAIN, "[HFISVC] Initializing context %d\n", ctx_index);
+
+	ret = (*domain->hfisvc.initialize)(domain->hfisvc.ctxs[ctx_index].ctx);
+	if (ret) {
+		FI_WARN(fi_opx_global.prov, FI_LOG_DOMAIN,
+			"[HFISVC] hfisvc_client_initialize failed for ctx %d, ret=%d\n", ctx_index, ret);
+		return -FI_ENODEV;
+	}
+
+	ret = (*domain->hfisvc.get_client_key)(domain->hfisvc.ctxs[ctx_index].ctx,
+					       &domain->hfisvc.ctxs[ctx_index].client_key);
+	if (ret) {
+		FI_WARN(fi_opx_global.prov, FI_LOG_DOMAIN, "[HFISVC] get_client_key failed for ctx %d, ret=%d\n",
+			ctx_index, ret);
+		return -FI_ENODEV;
+	}
+
+	OPX_HFISVC_DEBUG_LOG("Initializing hfisvc keyset for ctx %d\n", ctx_index);
+	ret = opx_hfisvc_keyset_init(&domain->hfisvc.ctxs[ctx_index].access_key_set);
+	if (ret) {
+		FI_WARN(fi_opx_global.prov, FI_LOG_DOMAIN, "[HFISVC] Failed initializing keyset for ctx %d, ret=%d\n",
+			ctx_index, ret);
+		return -FI_ENODEV;
+	}
+
+	FI_INFO(fi_opx_global.prov, FI_LOG_DOMAIN, "[HFISVC] Initialized context %d with client key %u\n", ctx_index,
+		domain->hfisvc.ctxs[ctx_index].client_key);
+
+	return FI_SUCCESS;
+}
+
 int opx_domain_hfisvc_init(struct fi_opx_domain *domain)
 {
 	int rc = FI_SUCCESS;
@@ -696,43 +757,37 @@ int opx_domain_hfisvc_init(struct fi_opx_domain *domain)
 	assert(domain->hfisvc.cmd_rdma_read_va != NULL);
 	assert(domain->hfisvc.doorbell != NULL);
 
-	int ret = (*domain->hfisvc.initialize)(domain->hfisvc.ctx);
+	/* Open domain-level MR command/completion queues on primary context */
+	OPX_HFISVC_DEBUG_LOG("Creating domain MR command queue\n");
+	int ret = (*domain->hfisvc.command_queue_open)(&domain->hfisvc.mr_command_queue, domain->hfisvc.ctxs[0].ctx);
 	if (ret) {
-		FI_WARN(fi_opx_global.prov, FI_LOG_DOMAIN,
-			"[HFISVC] hfisvc_client_initialize failed, disable hfisvc\n");
+		FI_WARN(fi_opx_global.prov, FI_LOG_DOMAIN, "[HFISVC] Failed creating domain MR command queue, ret=%d\n",
+			ret);
 		rc = -FI_ENODEV;
 		goto done;
 	}
-	ret = (*domain->hfisvc.get_client_key)(domain->hfisvc.ctx, &domain->hfisvc.client_key);
-	if (ret) {
-		abort();
-	}
-	OPX_HFISVC_DEBUG_LOG("Creating new domain mr command queue\n");
-	ret = (*domain->hfisvc.command_queue_open)(&domain->hfisvc.mr_command_queue, domain->hfisvc.ctx);
 
+	OPX_HFISVC_DEBUG_LOG("Creating domain MR completion queue\n");
+	ret = (*domain->hfisvc.completion_queue_open)(&domain->hfisvc.mr_completion_queue, domain->hfisvc.ctxs[0].ctx);
 	if (ret) {
-		fprintf(stderr, "(%d) %s:%s():%d Failed creating domain mr command queue! ret=%d\n", getpid(), __FILE__,
-			__func__, __LINE__, ret);
-		abort();
+		FI_WARN(fi_opx_global.prov, FI_LOG_DOMAIN,
+			"[HFISVC] Failed creating domain MR completion queue, ret=%d\n", ret);
+		(*domain->hfisvc.command_queue_close)(&domain->hfisvc.mr_command_queue);
+		rc = -FI_ENODEV;
+		goto done;
 	}
 
-	OPX_HFISVC_DEBUG_LOG("Creating new domain mr completion queue\n");
-	ret = (*domain->hfisvc.completion_queue_open)(&domain->hfisvc.mr_completion_queue, domain->hfisvc.ctx);
+	/* Initialize the primary context (ctxs[0]) */
+	ret = opx_domain_hfisvc_init_ctx(domain, 0);
+	if (ret) {
+		(*domain->hfisvc.completion_queue_close)(&domain->hfisvc.mr_completion_queue);
+		(*domain->hfisvc.command_queue_close)(&domain->hfisvc.mr_command_queue);
+		rc = ret;
+		goto done;
+	}
 
-	if (ret) {
-		fprintf(stderr, "(%d) %s:%s():%d Failed creating domain mr completion queue! ret=%d\n", getpid(),
-			__FILE__, __func__, __LINE__, ret);
-		abort();
-	}
-	OPX_HFISVC_DEBUG_LOG("Initializing hfisvc keyset\n");
-	ret = opx_hfisvc_keyset_init(&domain->hfisvc.access_key_set);
-	if (ret) {
-		fprintf(stderr, "(%d) %s:%s():%d Failed initializing hfisvc keyset! ret=%d\n", getpid(), __FILE__,
-			__func__, __LINE__, ret);
-		abort();
-	}
+	domain->hfisvc.num_ctxs = 1;
 	ofi_atomic_set64(&domain->hfisvc.ref_cnt, 1);
-	OPX_HFISVC_DEBUG_LOG("Initialized HFI service with client key %u\n", domain->hfisvc.client_key);
 	domain->use_hfisvc = 1;
 
 done:

prov/opx/include/opa_service.h
@@ -6,7 +6,7 @@
   GPL LICENSE SUMMARY
 
   Copyright(c) 2015 Intel Corporation.
-  Copyright(c) 2021-2025 Cornelis Networks.
+  Copyright(c) 2021-2026 Cornelis Networks.
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of version 2 of the GNU General Public License as
@@ -20,7 +20,7 @@
   BSD LICENSE
 
   Copyright(c) 2015 Intel Corporation.
-  Copyright(c) 2021-2025 Cornelis Networks.
+  Copyright(c) 2021-2026 Cornelis Networks.
 
   Redistribution and use in source and binary forms, with or without
   modification, are permitted provided that the following conditions
@@ -190,6 +190,10 @@ int opx_hfi_get_num_ports(int hfi_unit);
    returns 0 if no port on the unit is active.
    returns -1 when an error occurred. */
 int opx_hfi_get_unit_active(int unit);
+
+/* Given a unit number and port number, returns 1 if the unit and port are active.
+   returns 0 if the unit and port are not active or if the unit is not found. */
+int opx_hfi_get_unit_port_active(int unit, int port);
 
 /* Get the number of free contexts from the unit id. */
 /* Returns 0 if no unit or no match. */

prov/opx/include/rdma/opx/fi_opx.h
@@ -175,6 +175,8 @@ static const char *const OPX_HFI1_PACKET_STR[] = {
 #define OPX_MAX_HFIS (16)
 OPX_COMPILE_TIME_ASSERT((OPX_MAX_HFIS & 3) == 0, "OPX_MAX_HFIS must be a multiple of 4!\n");
 
+#define OPX_MAX_TX_CONTEXTS (2)
+
 struct opx_hfi_local_entry {
 	uint32_t instance;
 	uint8_t	 hfi_unit;

prov/opx/include/rdma/opx/fi_opx_endpoint.h
@@ -273,9 +273,13 @@ struct fi_opx_ep_tx {
 	struct slist	      work_pending[OPX_WORK_TYPE_LAST];
 	ofi_atomic64_t	      ref_cnt;
 	struct opx_spio_ctrl *spio_ctrl;
-	// struct opx_shm_tx is very large and should go last!
+	/* struct opx_shm_tx is very large and should go last! */
 	struct opx_shm_tx shm;
 	void		 *mem;
+
+	/* Multi-plane support: HFI context and plane identifier for this TX context */
+	struct fi_opx_hfi1_context *hfi;
+	uint64_t		    gid_hi;
 } __attribute__((__aligned__(L2_CACHE_LINE_SIZE))) __attribute__((__packed__));
 
 OPX_COMPILE_TIME_ASSERT(offsetof(struct fi_opx_ep_tx, inject_9B) == (FI_OPX_CACHE_LINE_SIZE * 1),
@@ -509,11 +513,15 @@ struct fi_opx_ep {
 	uint32_t	av_type;
 	uint32_t	mr_mode;
 	enum fi_ep_type type;
-	uint64_t	unused_cacheline3[3];
+
+	/* Multi-plane support: arrays of TX contexts */
+	struct fi_opx_ep_tx *tx_contexts[OPX_MAX_TX_CONTEXTS];
+	uint16_t	     num_tx_contexts;
+	uint16_t	     unused_cl3_1;
+	uint32_t	     unused_cl3_pad;
 
 	/* == CACHE LINE 4 == */
-	// Only used for initialization
-	// free these flags
+	/* Init-only fields: fi_info, CQ/counter bindings */
 	struct fi_info	   *common_info;
 	struct fi_info	   *tx_info;
 	struct fi_info	   *rx_info;
@@ -524,32 +532,24 @@ struct fi_opx_ep {
 	uint64_t	    rx_cq_bflags;
 
 	/* == CACHE LINE 5 == */
-	struct fi_opx_cntr	  *init_send_cntr;
-	struct fi_opx_cntr	  *init_recv_cntr;
-	uint64_t		   tx_cq_bflags;
-	struct fi_opx_ep_daos_info daos_info; /* 18 bytes */
-	bool			   is_tx_cq_bound;
-	bool			   is_rx_cq_bound;
-	bool			   use_expected_tid_rzv;
-	bool			   use_hfisvc;
-	uint8_t			   unused_cacheline5[2];
-	enum fi_hmem_iface	   use_gpu_ipc;
-	ofi_spin_t		   lock; /* lock size varies based on ENABLE_DEBUG*/
-
-	/* == CACHE LINE 6 == */
+	/* hfisvc is placed first so its offset is fixed regardless of
+	   ofi_spin_t size (which varies between DEBUG and RELEASE builds).
+	   Sized to exactly 64 bytes (one cacheline). */
 #if HAVE_HFISVC
 	struct {
 		/**
-		 * @brief Command queue used by this endpoint to submit commands to the hfisvc
+		 * @brief Command queues used by this endpoint to submit commands to the hfisvc
+		 * (one per plane for dual-plane support)
 		 */
-		hfisvc_client_command_queue_t command_queue;
+		hfisvc_client_command_queue_t command_queues[OPX_MAX_TX_CONTEXTS];
 
 		/**
-		 * @brief Completion queue used by the endpoint for handling completions from the
+		 * @brief Completion queues used by the endpoint for handling completions from the
 		 * hfisvc where we need to do additional work. For example, we may need to free certain
 		 * resources before sending a completion up to the user/middleware.
+		 * (one per plane for dual-plane support)
 		 */
-		hfisvc_client_completion_queue_t internal_completion_queue;
+		hfisvc_client_completion_queue_t internal_completion_queues[OPX_MAX_TX_CONTEXTS];
 
 		/**
 		 * @brief Pointer to the CQ's hfisvc completion queue. Use this completion queue
@@ -558,14 +558,36 @@ struct fi_opx_ep {
 		 */
 		hfisvc_client_completion_queue_t *cq_completion_queue;
 
-		uint32_t rdma_read_count;
-		uint32_t unused_dw;
-
-		uint64_t unused_qws[4];
+		uint32_t rdma_read_count[OPX_MAX_TX_CONTEXTS];
+		uint32_t num_queues;
+		uint32_t unused_pad;
+		uint64_t unused_qw;
 	} hfisvc;
+#else
+	/* Placeholder to maintain cacheline alignment when hfisvc is disabled */
+	uint8_t unused_cl5[FI_OPX_CACHE_LINE_SIZE];
 #endif
 
-	/* == CACHE LINE 7 (if ENABLE_DEBUG) == */
+	/* == CACHE LINE 6 == */
+	/* Hot-path scalars: accessed on every poll, send, recv, or match. */
+	struct fi_opx_ep_daos_info daos_info; /* 18 bytes */
+	bool			   use_expected_tid_rzv;
+	bool			   use_hfisvc;
+	enum fi_hmem_iface	   use_gpu_ipc;
+	ofi_spin_t		   lock; /* lock size varies based on ENABLE_DEBUG */
+
+	/* Init-only fields (cold): set during bind/enable, never on hot path */
+	struct fi_opx_cntr *init_send_cntr;
+	struct fi_opx_cntr *init_recv_cntr;
+	uint64_t	    tx_cq_bflags;
+	bool		    is_tx_cq_bound;
+	bool		    is_rx_cq_bound;
+	uint8_t		    unused_cl6[6];
+
+	/* Multi-plane: secondary HFI contexts (init/teardown only, not hot-path) */
+	struct fi_opx_hfi1_context *hfi_contexts[OPX_MAX_TX_CONTEXTS];
+
+	/* == CACHE LINE 7+ == */
 #ifdef FLIGHT_RECORDER_ENABLE
 	struct flight_recorder *fr;
 #endif
@@ -580,11 +602,15 @@ OPX_COMPILE_TIME_ASSERT(offsetof(struct fi_opx_ep, recv_cntr) == (FI_OPX_CACHE_L
 OPX_COMPILE_TIME_ASSERT(offsetof(struct fi_opx_ep, hfi) == (FI_OPX_CACHE_LINE_SIZE * 3),
 			"Offset of fi_opx_ep->hfi should start at cacheline 3!");
 OPX_COMPILE_TIME_ASSERT(offsetof(struct fi_opx_ep, common_info) == (FI_OPX_CACHE_LINE_SIZE * 4),
-			"Offset of fi_opx_ep->hfi should start at cacheline 4!");
-OPX_COMPILE_TIME_ASSERT(offsetof(struct fi_opx_ep, init_send_cntr) == (FI_OPX_CACHE_LINE_SIZE * 5),
-			"Offset of fi_opx_ep->init_send_cntr should start at cacheline 5!");
-OPX_COMPILE_TIME_ASSERT(offsetof(struct fi_opx_ep, lock) == ((FI_OPX_CACHE_LINE_SIZE * 5) + 52),
-			"Offset of fi_opx_ep->lock should start before cacheline 6!");
+			"Offset of fi_opx_ep->common_info should start at cacheline 4!");
+#if HAVE_HFISVC
+OPX_COMPILE_TIME_ASSERT(offsetof(struct fi_opx_ep, hfisvc) == (FI_OPX_CACHE_LINE_SIZE * 5),
+			"Offset of fi_opx_ep->hfisvc should start at cacheline 5!");
+OPX_COMPILE_TIME_ASSERT(sizeof(((struct fi_opx_ep *) 0)->hfisvc) == FI_OPX_CACHE_LINE_SIZE,
+			"fi_opx_ep->hfisvc must be exactly one cacheline!");
+#endif
+OPX_COMPILE_TIME_ASSERT(offsetof(struct fi_opx_ep, daos_info) == (FI_OPX_CACHE_LINE_SIZE * 6),
+			"Offset of fi_opx_ep->daos_info should start at cacheline 6!");
 
 /*
  * A 'scalable endpoint' may not be directly specified in a data movement
@@ -1123,13 +1149,13 @@ void fi_opx_handle_recv_rts_hfisvc(const union opx_hfi1_packet_hdr *const	 hdr,
 			int	       rc;
 			if (do_dmabuf) {
 				rc = (*opx_ep->domain->hfisvc.cmd_rdma_read)(
-					opx_ep->hfisvc.command_queue, completion, 0ul /* flags */, lid, sbuf_key,
+					opx_ep->hfisvc.command_queues[0], completion, 0ul /* flags */, lid, sbuf_key,
 					sbuf_len, payload->rendezvous.hfisvc.rzv_comp_vaddr, sbuf_access_key,
 					sbuf_offset, opx_mr->hfisvc.mr_handle,
 					opx_mr_dmabuf_local_offset(opx_mr, recv_buf));
 			} else {
 				rc = (*opx_ep->domain->hfisvc.cmd_rdma_read_va)(
-					opx_ep->hfisvc.command_queue, completion, 0ul /* flags */, lid, sbuf_key,
+					opx_ep->hfisvc.command_queues[0], completion, 0ul /* flags */, lid, sbuf_key,
 					sbuf_len, payload->rendezvous.hfisvc.rzv_comp_vaddr, sbuf_access_key,
 					sbuf_offset, recv_buf);
 			}
@@ -1154,7 +1180,7 @@ void fi_opx_handle_recv_rts_hfisvc(const union opx_hfi1_packet_hdr *const	 hdr,
 				// but only by 1 down below right before recv_rts_hfisvc_finish is that for the
 				// case of i=0, we don't want to actually increment the count. Always incrementing
 				// by 'i' here is just more performant/less branchy than doing '+= i ? 1 : 0'
-				opx_ep->hfisvc.rdma_read_count += i;
+				opx_ep->hfisvc.rdma_read_count[0] += i;
 				slist_insert_tail((struct slist_entry *) context, rx->cq_pending_ptr);
 
 				goto recv_rts_hfisvc_finish;
@@ -1182,12 +1208,12 @@ void fi_opx_handle_recv_rts_hfisvc(const union opx_hfi1_packet_hdr *const	 hdr,
 			int	       rc;
 			if (do_dmabuf) {
 				rc = (*opx_ep->domain->hfisvc.cmd_rdma_read)(
-					opx_ep->hfisvc.command_queue, completion, 0ul /* flags */, lid, sbuf_key,
+					opx_ep->hfisvc.command_queues[0], completion, 0ul /* flags */, lid, sbuf_key,
 					1ul /* length */, payload->rendezvous.hfisvc.rzv_comp_vaddr, sbuf_access_key,
 					0ul /* remote offset */, opx_mr->hfisvc.mr_handle, 0);
 			} else {
 				rc = (*opx_ep->domain->hfisvc.cmd_rdma_read_va)(
-					opx_ep->hfisvc.command_queue, completion, 0ul /* flags */, lid, sbuf_key,
+					opx_ep->hfisvc.command_queues[0], completion, 0ul /* flags */, lid, sbuf_key,
 					1ul /* length */, payload->rendezvous.hfisvc.rzv_comp_vaddr, sbuf_access_key,
 					0ul /* remote offset */, &opx_trunc_scratch_buf);
 			}
@@ -1214,7 +1240,7 @@ void fi_opx_handle_recv_rts_hfisvc(const union opx_hfi1_packet_hdr *const	 hdr,
 						  recv_buf);
 	}
 
-	++opx_ep->hfisvc.rdma_read_count;
+	++opx_ep->hfisvc.rdma_read_count[0];
 recv_rts_hfisvc_finish:
 	OPX_TRACER_TRACE(OPX_TRACER_END_SUCCESS, "RECV-RZV-RTS-HFISVC");
 	FI_DBG_TRACE(fi_opx_global.prov, FI_LOG_EP_DATA,
@@ -2087,7 +2113,7 @@ ssize_t fi_opx_shm_dynamic_tx_connect(const unsigned is_shm, struct fi_opx_ep *o
 	uint32_t segment_index = OPX_SHM_SEGMENT_INDEX(hfi1_unit, rx_id);
 #endif
 
-	struct opx_shm_info *shm_info = opx_shm_rbt_get_shm_info(&(opx_ep->tx->shm), segment_index);
+	struct opx_shm_info *shm_info = opx_shm_rbt_get_shm_info(&opx_ep->tx->shm, segment_index);
 
 	if (OFI_LIKELY((shm_info != NULL) && (shm_info->fifo_segment != NULL))) {
 		/* Connection already established */
@@ -3296,7 +3322,8 @@ void fi_opx_ep_do_pending_work(struct fi_opx_ep *opx_ep)
 
 #if HAVE_HFISVC
 __OPX_FORCE_INLINE__
-void opx_ep_hfisvc_poll_proc_internal_completion(struct fi_opx_ep *opx_ep, struct hfisvc_client_cq_entry *hfisvc_entry)
+void opx_ep_hfisvc_poll_proc_internal_completion(struct fi_opx_ep *opx_ep, struct hfisvc_client_cq_entry *hfisvc_entry,
+						 int ctx_idx)
 {
 	struct fi_opx_rzv_completion *rzv_comp = (struct fi_opx_rzv_completion *) hfisvc_entry->app_context;
 	assert(rzv_comp);
@@ -3345,7 +3372,7 @@ void opx_ep_hfisvc_poll_proc_internal_completion(struct fi_opx_ep *opx_ep, struc
 		}
 	}
 
-	opx_hfisvc_keyset_free_key(opx_ep->domain->hfisvc.access_key_set, rzv_comp->access_key,
+	opx_hfisvc_keyset_free_key(opx_ep->domain->hfisvc.ctxs[ctx_idx].access_key_set, rzv_comp->access_key,
 				   FI_OPX_DEBUG_COUNTERS_GET_PTR(opx_ep));
 	OPX_BUF_FREE(rzv_comp);
 }
@@ -3361,14 +3388,16 @@ void opx_ep_hfisvc_poll_internal_queue(struct fi_opx_ep *opx_ep)
 
 	struct hfisvc_client_cq_entry hfisvc_out[64];
 
-	size_t n = (*opx_ep->domain->hfisvc.cq_read)(opx_ep->hfisvc.internal_completion_queue, 0ul /* flags */,
-						     hfisvc_out, sizeof(*hfisvc_out), 64);
-	while (n > 0) {
-		for (size_t i = 0; i < n; ++i) {
-			opx_ep_hfisvc_poll_proc_internal_completion(opx_ep, &hfisvc_out[i]);
+	for (int ctx_idx = 0; ctx_idx < opx_ep->hfisvc.num_queues; ctx_idx++) {
+		size_t n = (*opx_ep->domain->hfisvc.cq_read)(opx_ep->hfisvc.internal_completion_queues[ctx_idx],
+							     0ul /* flags */, hfisvc_out, sizeof(*hfisvc_out), 64);
+		while (n > 0) {
+			for (size_t i = 0; i < n; ++i) {
+				opx_ep_hfisvc_poll_proc_internal_completion(opx_ep, &hfisvc_out[i], ctx_idx);
+			}
+			n = (*opx_ep->domain->hfisvc.cq_read)(opx_ep->hfisvc.internal_completion_queues[ctx_idx],
+							      0ul /* flags */, hfisvc_out, sizeof(*hfisvc_out), 64);
 		}
-		n = (*opx_ep->domain->hfisvc.cq_read)(opx_ep->hfisvc.internal_completion_queue, 0ul /* flags */,
-						      hfisvc_out, sizeof(*hfisvc_out), 64);
 	}
 #endif
 }
@@ -3599,7 +3628,10 @@ int fi_opx_ep_rx_process_context(struct fi_opx_ep *opx_ep, const uint64_t static
 				 const enum ofi_reliability_kind reliability, const enum opx_hfi1_type hfi1_type)
 {
 #if HAVE_HFISVC
-	uint32_t start_rdma_read_count = opx_ep->hfisvc.rdma_read_count;
+	uint32_t start_rdma_read_count[OPX_MAX_TX_CONTEXTS];
+	for (int i = 0; i < opx_ep->hfisvc.num_queues; i++) {
+		start_rdma_read_count[i] = opx_ep->hfisvc.rdma_read_count[i];
+	}
 #endif
 	int rc;
 
@@ -3624,14 +3656,15 @@ int fi_opx_ep_rx_process_context(struct fi_opx_ep *opx_ep, const uint64_t static
 	}
 
 #if HAVE_HFISVC
-	if (opx_ep->hfisvc.rdma_read_count != start_rdma_read_count) {
-		OPX_HFISVC_DEBUG_LOG(
-			"Rang doorbell because start_rdma_read_count=%u and opx_ep->hfisvc.rdma_read_count=%u\n",
-			start_rdma_read_count, opx_ep->hfisvc.rdma_read_count);
-		int doorbell_rc __attribute__((unused));
-		doorbell_rc = (*opx_ep->domain->hfisvc.doorbell)(opx_ep->domain->hfisvc.ctx);
-
-		assert(doorbell_rc == 0);
+	for (int i = 0; i < opx_ep->hfisvc.num_queues; i++) {
+		if (opx_ep->hfisvc.rdma_read_count[i] != start_rdma_read_count[i]) {
+			OPX_HFISVC_DEBUG_LOG(
+				"Rang doorbell because start_rdma_read_count=%u and opx_ep->hfisvc.rdma_read_count=%u for plane %d\n",
+				start_rdma_read_count[i], opx_ep->hfisvc.rdma_read_count[i], i);
+			int doorbell_rc __attribute__((unused));
+			doorbell_rc = (*opx_ep->domain->hfisvc.doorbell)(opx_ep->domain->hfisvc.ctxs[i].ctx);
+			assert(doorbell_rc == 0);
+		}
 	}
 #endif
 

prov/opx/include/rdma/opx/fi_opx_hfi1.h
@@ -619,6 +619,17 @@ struct fi_opx_hfi1_context_internal {
 	struct _hfi_ctrl	 *ctrl;
 };
 
+struct fi_opx_plane_info {
+	uint32_t hfi_unit;
+	uint32_t hfi_port;
+	uint64_t gid_hi;
+};
+
+int fi_opx_hfi1_discover_planes(struct fi_opx_hfi1_context *primary_hfi, struct fi_opx_plane_info *planes,
+				int max_planes);
+
+struct fi_opx_hfi1_context *fi_opx_hfi1_context_open_unit(struct fid_ep *ep, uuid_t unique_job_key, uint32_t hfi_unit);
+
 #ifdef NDEBUG
 #define FI_OPX_HFI1_CHECK_CREDITS_FOR_ERROR(credits_addr)
 #else

commit b5c7418f8940ee57c7d54a5ca16e3d70b884c8d3

prov/opx: Initialize HFISVC client before opening MR queues
The HFISVC domain init path opened the domain-level MR command and
completion queues before initializing the primary HFISVC client context.
That ordering violates the hfi1/libhfi1verbs contract: BULKSVC_GET_CMDQ
and BULKSVC_GET_CMPLQ require BULKSVC_CLIENT_INIT to have created
fd->bulksvc_user_info first.

After the dual-plane context refactor, OPX therefore called GET_CMDQ on
ctxs[0].ctx before hfisvc_client_initialize(), causing the kernel uverbs
handler to return -EINVAL. libhfi1verbs surfaced that as ret=22, and OPX
disabled HFISVC:

    [HFISVC] Failed creating domain MR command queue, ret=22
    [HFISVC] disabled hfisvc

Move primary context initialization ahead of the domain MR queue opens so
CLIENT_INIT runs before GET_CMDQ/GET_CMPLQ. Add hfisvc_client_finalize()
handling on failure and domain close paths so partial initialization is
cleaned up symmetrically.

Fixes: 845e42673dfa ("prov/opx: Dual plane: Multiple tx and ibv context")

Signed-off-by: Lindsay Reiser <lindsay.reiser@cornelisnetworks.com>

prov/opx/include/rdma/opx/fi_opx_domain.h
@@ -141,6 +141,7 @@ struct fi_opx_domain {
 		ofi_atomic64_t			 ref_cnt;
 		void				*libhfi1verbs;
 		int (*initialize)(struct ibv_context *ctx);
+		int (*finalize)(struct ibv_context *ctx);
 		int (*get_client_key)(struct ibv_context *ctx, hfisvc_client_key_t *key);
 		int (*command_queue_open)(hfisvc_client_command_queue_t *command_queue, struct ibv_context *ctx);
 		int (*command_queue_close)(hfisvc_client_command_queue_t *command_queue);

prov/opx/src/fi_opx_domain.c
@@ -140,6 +140,12 @@ static int fi_opx_close_domain(fid_t fid)
 			FI_INFO(fi_opx_global.prov, FI_LOG_DOMAIN, "[HFISVC] Closing context %d\n", i);
 
 			opx_hfisvc_keyset_free(opx_domain->hfisvc.ctxs[i].access_key_set);
+			opx_domain->hfisvc.ctxs[i].access_key_set = 0;
+			int finalize_ret = (*opx_domain->hfisvc.finalize)(opx_domain->hfisvc.ctxs[i].ctx);
+			if (finalize_ret) {
+				FI_WARN(fi_opx_global.prov, FI_LOG_DOMAIN,
+					"[HFISVC] Failed finalizing context %d, ret=%d\n", i, finalize_ret);
+			}
 			opx_hfi1_rdma_context_close(opx_domain->hfisvc.ctxs[i].ctx);
 			opx_domain->hfisvc.ctxs[i].ctx = NULL;
 		}
@@ -684,6 +690,11 @@ int opx_domain_hfisvc_init_ctx(struct fi_opx_domain *domain, int ctx_index)
 	if (ret) {
 		FI_WARN(fi_opx_global.prov, FI_LOG_DOMAIN, "[HFISVC] get_client_key failed for ctx %d, ret=%d\n",
 			ctx_index, ret);
+		int finalize_ret = (*domain->hfisvc.finalize)(domain->hfisvc.ctxs[ctx_index].ctx);
+		if (finalize_ret) {
+			FI_WARN(fi_opx_global.prov, FI_LOG_DOMAIN, "[HFISVC] Failed finalizing context %d, ret=%d\n",
+				ctx_index, finalize_ret);
+		}
 		return -FI_ENODEV;
 	}
 
@@ -692,6 +703,11 @@ int opx_domain_hfisvc_init_ctx(struct fi_opx_domain *domain, int ctx_index)
 	if (ret) {
 		FI_WARN(fi_opx_global.prov, FI_LOG_DOMAIN, "[HFISVC] Failed initializing keyset for ctx %d, ret=%d\n",
 			ctx_index, ret);
+		int finalize_ret = (*domain->hfisvc.finalize)(domain->hfisvc.ctxs[ctx_index].ctx);
+		if (finalize_ret) {
+			FI_WARN(fi_opx_global.prov, FI_LOG_DOMAIN, "[HFISVC] Failed finalizing context %d, ret=%d\n",
+				ctx_index, finalize_ret);
+		}
 		return -FI_ENODEV;
 	}
 
@@ -735,6 +751,7 @@ int opx_domain_hfisvc_init(struct fi_opx_domain *domain)
 
 	FI_WARN(fi_opx_global.prov, FI_LOG_DOMAIN, "[HFISVC] hfisvc_client_initialize found\n");
 
+	domain->hfisvc.finalize		   = dlsym(domain->hfisvc.libhfi1verbs, "hfisvc_client_finalize");
 	domain->hfisvc.get_client_key	   = dlsym(domain->hfisvc.libhfi1verbs, "hfisvc_client_key");
 	domain->hfisvc.command_queue_open  = dlsym(domain->hfisvc.libhfi1verbs, "hfisvc_client_command_queue_open");
 	domain->hfisvc.command_queue_close = dlsym(domain->hfisvc.libhfi1verbs, "hfisvc_client_command_queue_close");
@@ -757,6 +774,7 @@ int opx_domain_hfisvc_init(struct fi_opx_domain *domain)
 		dlsym(domain->hfisvc.libhfi1verbs, "hfisvc_client_cmd_dma_access_disable");
 	domain->hfisvc.doorbell = dlsym(domain->hfisvc.libhfi1verbs, "hfisvc_client_doorbell");
 
+	assert(domain->hfisvc.finalize != NULL);
 	assert(domain->hfisvc.get_client_key != NULL);
 	assert(domain->hfisvc.command_queue_open != NULL);
 	assert(domain->hfisvc.command_queue_close != NULL);
@@ -767,12 +785,26 @@ int opx_domain_hfisvc_init(struct fi_opx_domain *domain)
 	assert(domain->hfisvc.cmd_rdma_read_va != NULL);
 	assert(domain->hfisvc.doorbell != NULL);
 
+	/* Initialize the primary context (ctxs[0]) before opening queues. */
+	int ret = opx_domain_hfisvc_init_ctx(domain, 0);
+	if (ret) {
+		rc = ret;
+		goto done;
+	}
+
 	/* Open domain-level MR command/completion queues on primary context */
 	OPX_HFISVC_DEBUG_LOG("Creating domain MR command queue\n");
-	int ret = (*domain->hfisvc.command_queue_open)(&domain->hfisvc.mr_command_queue, domain->hfisvc.ctxs[0].ctx);
+	ret = (*domain->hfisvc.command_queue_open)(&domain->hfisvc.mr_command_queue, domain->hfisvc.ctxs[0].ctx);
 	if (ret) {
 		FI_WARN(fi_opx_global.prov, FI_LOG_DOMAIN, "[HFISVC] Failed creating domain MR command queue, ret=%d\n",
 			ret);
+		opx_hfisvc_keyset_free(domain->hfisvc.ctxs[0].access_key_set);
+		domain->hfisvc.ctxs[0].access_key_set = 0;
+		int finalize_ret		      = (*domain->hfisvc.finalize)(domain->hfisvc.ctxs[0].ctx);
+		if (finalize_ret) {
+			FI_WARN(fi_opx_global.prov, FI_LOG_DOMAIN,
+				"[HFISVC] Failed finalizing primary context, ret=%d\n", finalize_ret);
+		}
 		rc = -FI_ENODEV;
 		goto done;
 	}
@@ -783,16 +815,14 @@ int opx_domain_hfisvc_init(struct fi_opx_domain *domain)
 		FI_WARN(fi_opx_global.prov, FI_LOG_DOMAIN,
 			"[HFISVC] Failed creating domain MR completion queue, ret=%d\n", ret);
 		(*domain->hfisvc.command_queue_close)(&domain->hfisvc.mr_command_queue);
+		opx_hfisvc_keyset_free(domain->hfisvc.ctxs[0].access_key_set);
+		domain->hfisvc.ctxs[0].access_key_set = 0;
+		int finalize_ret		      = (*domain->hfisvc.finalize)(domain->hfisvc.ctxs[0].ctx);
+		if (finalize_ret) {
+			FI_WARN(fi_opx_global.prov, FI_LOG_DOMAIN,
+				"[HFISVC] Failed finalizing primary context, ret=%d\n", finalize_ret);
+		}
 		rc = -FI_ENODEV;
-		goto done;
-	}
-
-	/* Initialize the primary context (ctxs[0]) */
-	ret = opx_domain_hfisvc_init_ctx(domain, 0);
-	if (ret) {
-		(*domain->hfisvc.completion_queue_close)(&domain->hfisvc.mr_completion_queue);
-		(*domain->hfisvc.command_queue_close)(&domain->hfisvc.mr_command_queue);
-		rc = ret;
 		goto done;
 	}
 